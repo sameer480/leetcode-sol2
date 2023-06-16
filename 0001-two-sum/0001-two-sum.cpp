33572de23301e68be9f4d143c74bab036f2f71cb
Sameer Jawed <90494125+sameer480@users.noreply.github.com>
2023-06-17 02:57:54 +0530

    Time: 16 ms (61.47%), Space: 11.9 MB (9.27%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        vector<int> v;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (m.find(target - nums[i]) != m.end()) {
+                v.push_back(i);
+                v.push_back(m[target - nums[i]]);
+            } else {
+                m[nums[i]] = i;
+            }
+        }
+        return v;
+    }
+};
