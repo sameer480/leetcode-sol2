47da3f9fdceecb78a5c8dfebfd3972af58603e20
Sameer Jawed <90494125+sameer480@users.noreply.github.com>
2023-06-16 21:22:32 +0530

    Time: 19 ms (93.51%), Space: 19.6 MB (7.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int lastzero=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0){
-                nums[lastzero]=nums[i];
-                lastzero++;
+        int n = nums.size();
+        vector<int> nzero(n, 0); // Create a new vector to store non-zero elements
+        
+        int j = 0; // Index for non-zero elements
+        
+        // Move all non-zero elements to the new vector
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nzero[j] = nums[i];
+                j++;
             }
         }
-        for(int i=lastzero;i<nums.size();i++){
-            nums[i]=0;
-        }   
-       
+        
+        // Fill the remaining elements with zeroes
+        while (j < n) {
+            nzero[j] = 0;
+            j++;
+        }
+        
+        // Copy the modified vector back to the original vector
+        nums = nzero;
     }
 };
