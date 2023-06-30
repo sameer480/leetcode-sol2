d755fb7f28bbd9349471e905caa5cfc805ae6159
Sameer Jawed <90494125+sameer480@users.noreply.github.com>
2023-07-01 00:38:28 +0530

    Time: 0 ms (100%), Space: 7.8 MB (46.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
- int Help(vector<int>& nums, int index, vector<int>& dp) {
-    if (index == 0) return nums[0];
-    if (index < 0) return 0;
-    if (dp[index] != -1) return dp[index];
-    
-    int inc = nums[index] + Help(nums, index - 2, dp);
-    int exc = Help(nums, index - 1, dp);
-    return dp[index] = max(inc, exc);
-}
+  int Help(vector<int>& nums,int index, vector<int>&dp){
+     if(index==0) return nums[0] ;
+     if(index<0)return 0 ; 
+     if(dp[index]!=-1) return dp[index] ; 
+     int inc=nums[index]+Help(nums,index-2,dp) ; 
+     int exc=Help(nums,index-1,dp) ; 
+     return dp[index]=max(inc,exc) ; 
+ }
 
-int rob(vector<int>& nums) {
-    // if (nums.empty()) return 0;
-    vector<int> dp(nums.size(), -1);
-    return Help(nums, nums.size() - 1, dp);
-}
+    int rob(vector<int>& nums) {
+    vector<int>dp(nums.size(),-1) ;
+     return Help(nums,nums.size()-1,dp) ;
+    }
 };
