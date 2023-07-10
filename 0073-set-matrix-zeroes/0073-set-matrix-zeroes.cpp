725537e3120fe0829cb45e8db74ceb8cf5c8eea4
Sameer Jawed <90494125+sameer480@users.noreply.github.com>
2023-07-10 20:42:55 +0530

    Time: 17 ms (53.98%), Space: 13.7 MB (8.4%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -8,7 +8,7 @@ public:
     vector<int> row(n),col(m);
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(!matrix[i][j]){
+            if(matrix[i][j]==0){
                 col[j]=1;
                 row[i]=1;
 
