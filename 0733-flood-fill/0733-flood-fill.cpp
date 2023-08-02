741e389edb9ccbcc64394035cc3e19b6d183f7e6
Sameer Jawed <90494125+sameer480@users.noreply.github.com>
2023-08-03 00:06:21 +0530

    Time: 6 ms (87.97%), Space: 14 MB (63.65%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+void dfs(vector<vector<int>>& image, int sr, int sc, int color,int rows,int cols,int source){
+    if(sr<0||sr>=rows||sc<0||sc>=cols){
+        return;
+    }
+    else if(image[sr][sc]!=source)
+    return;
+    image[sr][sc]=color;
+    dfs(image,sr-1,sc,color,rows,cols,source);
+     dfs(image,sr+1,sc,color,rows,cols,source);
+      dfs(image,sr,sc+1,color,rows,cols,source);
+       dfs(image,sr,sc-1,color,rows,cols,source);
+}
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if(color==image[sr][sc]){
+            return image;
+        }
+        int rows=image.size();
+        int cols=image[0].size();
+        int source=image[sr][sc];
+        dfs(image,sr,sc,color,rows,cols,source);
+        return image;
+        
+    }
+};
